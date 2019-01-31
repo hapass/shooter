bd230223bac47677d02211c8cd90967c02c5c644
Pavel Zavoiskikh <hapass@ya.ru>
2019-02-01 02:32:12 +0300

    Setup and run in one line.

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 
 1. Install [xcode](https://developer.apple.com/xcode/)
 1. Install [brew](https://brew.sh)
-1. Setup. Terminal: *./setup.sh*
-1. Build. Terminal: *./build.sh Release*
-1. Run. Terminal: *open build/Release/shooter.app*
+1. Run. Terminal: *./setup.sh*
--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
-# engine_path="lib/murl"
-# engine_url="https://download.murlengine.com/murl_2018.2.7478.zip"
+engine_path="lib/murl"
+engine_url="https://download.murlengine.com/murl_2018.2.7478.zip"
 
-# mkdir -p $engine_path
-# cd $engine_path
+mkdir -p $engine_path
+cd $engine_path
 
-# curl $engine_url --output murl.zip
-# unzip murl.zip
-# rm murl.zip
+curl $engine_url --output murl.zip
+unzip murl.zip
+rm murl.zip
 
-# cd $OLDPWD
+cd $OLDPWD
 
-# rm -f CMakeProperties.txt
-# echo "set(murl_PATH \"$PWD/$engine_path/murl\" CACHE STRING \"path to murl engine\")" > CMakeProperties.txt
+rm -f CMakeProperties.txt
+echo "set(murl_PATH \"$PWD/$engine_path/murl\" CACHE STRING \"path to murl engine\")" > CMakeProperties.txt
 
 brew list clang-format &>/dev/null || brew install clang-format
 brew list cmake &>/dev/null || brew install cmake
+
+./build.sh Release
+open build/Release/shooter.app
